a0b2d73ba238e41346c2c7e97087d1ab24259d26
Jonatas <jonatas.07022003@gmail.com>
2024-01-24 17:05:47 -0300

    Criando os efeitos de editar, salvar e cancelar edição

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,35 +1,101 @@
 import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+
 import { Contato } from '../../App'
 import { BotaoEditarSalvarCadastrar, BotaoExcluirCancelar } from '../../styles'
 import * as S from './styles'
-import { remover } from '../../store/reducers/contato'
+import { editar, remover } from '../../store/reducers/contato'
 
 type Props = Contato
 
-const ContatoCard = ({ nome, email, telefone, id }: Props) => {
+const ContatoCard = ({
+  nome: nomeOriginal,
+  email: emailOriginal,
+  telefone: telefoneOriginal,
+  id
+}: Props) => {
   const dispatch = useDispatch()
+  const [editando, setEditando] = useState(false)
+
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [telefone, setTelefone] = useState('')
+
+  useEffect(() => {
+    if (
+      nomeOriginal.length > 0 ||
+      emailOriginal.length > 0 ||
+      telefoneOriginal.length > 0
+    ) {
+      setNome(nomeOriginal)
+      setEmail(emailOriginal)
+      setTelefone(telefoneOriginal)
+    }
+  }, [emailOriginal, nomeOriginal, telefoneOriginal])
+
+  function cancelarEdicao() {
+    setEditando(false)
+    setNome(nomeOriginal)
+    setEmail(emailOriginal)
+    setTelefone(telefoneOriginal)
+  }
 
   return (
     <S.Card>
       <div>
         <S.Dados>
           <label htmlFor="">Nome:</label>
-          <input type="text" value={nome} />
+          <input
+            type="text"
+            value={nome}
+            disabled={!editando}
+            onChange={(e) => setNome(e.target.value)}
+          />
         </S.Dados>
         <S.Dados>
           <label htmlFor="">E-mail:</label>
-          <input type="email" value={email} />
+          <input
+            type="email"
+            value={email}
+            disabled={!editando}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </S.Dados>
         <S.Dados>
           <label htmlFor="">Telefone:</label>
-          <input type="tel" value={telefone} />
+          <input
+            type="tel"
+            value={telefone}
+            disabled={!editando}
+            onChange={(e) => setTelefone(e.target.value)}
+          />
         </S.Dados>
       </div>
       <S.Buttons>
-        <BotaoEditarSalvarCadastrar>Editar</BotaoEditarSalvarCadastrar>
-        <BotaoExcluirCancelar onClick={() => dispatch(remover(id))}>
-          Remover
-        </BotaoExcluirCancelar>
+        {editando ? (
+          <>
+            <BotaoEditarSalvarCadastrar
+              onClick={() => {
+                dispatch(editar({ nome, email, telefone, id }))
+                setEditando(false)
+              }}
+            >
+              Salvar
+            </BotaoEditarSalvarCadastrar>
+            <BotaoExcluirCancelar onClick={cancelarEdicao}>
+              Cancelar
+            </BotaoExcluirCancelar>
+          </>
+        ) : (
+          <>
+            <BotaoEditarSalvarCadastrar onClick={() => setEditando(true)}>
+              Editar
+            </BotaoEditarSalvarCadastrar>
+            <BotaoExcluirCancelar onClick={() => dispatch(remover(id))}>
+              Remover
+            </BotaoExcluirCancelar>
+          </>
+        )}
       </S.Buttons>
     </S.Card>
   )
--- src/store/reducers/contato.ts
+++ src/store/reducers/contato.ts
@@ -37,9 +37,18 @@ const contatoSlice = createSlice({
       state.itens = state.itens.filter(
         (contato) => contato.id !== action.payload
       )
+    },
+    editar: (state, action: PayloadAction<Contato>) => {
+      const indexContato = state.itens.findIndex(
+        (contato) => contato.id === action.payload.id
+      )
+
+      if (indexContato >= 0) {
+        state.itens[indexContato] = action.payload
+      }
     }
   }
 })
 
-export const { cadastrar, remover } = contatoSlice.actions
+export const { cadastrar, remover, editar } = contatoSlice.actions
 export default contatoSlice.reducer
